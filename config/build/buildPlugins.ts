5f05079879d9f83cf601c70c1703d11513a5cac6
Efim-Kapliy <efim0424@mail.ru>
2024-04-10 05:34:05 +0300

    refactor: apply decomposition of the webpack config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,8 @@
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+import { BuildOptions } from './types/config';
+
+export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [new webpack.ProgressPlugin(), new HtmlWebpackPlugin({ template: paths.html })];
+}
