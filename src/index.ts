277c5d38dba109d5a78e3e21b6e716c95e09d2ba
Efim-Kapliy <efim0424@mail.ru>
2024-04-12 05:41:08 +0300

    feat: add and configure Webpack-dev-server, as well as production and development builds

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { someFn } from './test';
 
 someFn(52);
+
+document.body.innerHTML = `<div style="display: flex; justify-content: center; font-size: 24px;">Hello world!</div>`;
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 export function someFn(arg: number): string {
-  console.log('RENDOM FUNCTION dafasdfasdf');
+  console.log('RENDOM FUNCTION dafasdfasdf 4765474567');
 
   return '';
 }
