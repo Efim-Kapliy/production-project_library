a29887f565b19f19844655c8d3927db735ebc6cb
Efim-Kapliy <efim0424@mail.ru>
2024-05-02 16:06:11 +0300

    feat: add helper (classNames) to the project for working with css classes

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,11 @@
+type Mods = Record<string, boolean | string>;
+
+export function classNames(csl: string, mods: Mods, additional: string[]): string {
+  return [
+    csl,
+    ...Object.entries(mods)
+      .filter(([className, value]) => Boolean(value === true))
+      .map(([className]) => className),
+    ...additional,
+  ].join(' ');
+}
