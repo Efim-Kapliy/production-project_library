b244a275cc8bf8e70a1bedbab1ff998d05e038dc
Efim-Kapliy <efim0424@mail.ru>
2024-05-03 12:42:18 +0300

    feat: implement the configuration of the folder file structure according to the FSD architecture. Add absolute imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { useTheme } from 'app/providers/ThemeProvider';
 
 import './styles/index.scss';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -18,8 +18,8 @@ const App = () => {
       <Link to='/about'>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageAsync />} />
-          <Route path='/' element={<MainPageAsync />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/' element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
