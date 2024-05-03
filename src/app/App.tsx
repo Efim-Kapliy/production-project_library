6052619c69bbbe4736bd3c9885cae8023ec3e7d1
Efim-Kapliy <efim0424@mail.ru>
2024-05-03 16:09:00 +0300

    feat: add routing configuration to the project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
-import { Suspense } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
 
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -16,12 +14,7 @@ const App = () => {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='/' element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+
+const AppRouter = () => {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {Object.values(routeConfig).map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export { AppRouter };
