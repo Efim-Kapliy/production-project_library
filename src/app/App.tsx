56604cb3c97fdf739b9d744221fec77820865204
Efim-Kapliy <efim0424@mail.ru>
2024-05-05 19:38:47 +0300

    feat: add styles for navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,19 +2,20 @@ import { Link } from 'react-router-dom';
 
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 import './styles/index.scss';
-import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to='/'>Главная</Link>
-      <Link to='/about'>О сайте</Link>
+      <Navbar />
+
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
